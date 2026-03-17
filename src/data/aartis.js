71b0af19c489a9ddb9a20b8fedad04b40824cdb8
Monish Mahesh Gori <mmg@Monishs-MacBook-Air.local>
2026-03-17 20:50:43 +0530

    feat: point aarti audio back to aarti.mp3 after user replacement

--- src/data/aartis.js
+++ src/data/aartis.js
@@ -5,7 +5,7 @@ export const aartis = [
         gujarati: "|| શ્રી સોદેવપીર દાદાની આરતી ||",
         hindi: "|| श्री सोदेवपीर दादा की आरती ||",
             english: "|| shri sodevapira dada ki arati ||",
-        audio: "/assets/audio/new_aarti.m4a"
+        audio: "/assets/audio/aarti.mp3"
     },
     {
         id: "a2",
