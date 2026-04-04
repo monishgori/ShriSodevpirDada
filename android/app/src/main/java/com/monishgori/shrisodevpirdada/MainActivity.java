331e7b7fc9be4a05eafc94f5251a716104e5def3
Monish Mahesh Gori <mmg@Monishs-MacBook-Air.local>
2026-04-04 12:19:00 +0530

    Fix: Corrected onResume visibility to public for Java compliance 🔧🏺

--- android/app/src/main/java/com/monishgori/shrisodevpirdada/MainActivity.java
+++ android/app/src/main/java/com/monishgori/shrisodevpirdada/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         showAdIfAvailable();
     }
